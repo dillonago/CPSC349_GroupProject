88bb419e7e2f988f49a8b0b8d13eaaeeb9300a8a
dillonago <71573711+dillonago@users.noreply.github.com>
2021-11-01 17:35:01 -0700

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        max: 100,
+        min: 4
+    }, 
+    name: {
+        type: String, 
+        max: 100,
+        min: 4
+    }, 
+    bio: {
+        type: String, 
+        min: 10
+    },
+    type: {
+        type: String, 
+        min: 4
+    },
+    email: {
+        type: String, 
+        min: 6, 
+        max: 200
+    }, 
+    password: {
+        type: String, 
+        max: 1024, 
+        min: 6
+    },
+    date: {
+        type: Date, 
+        default: Date.now
+    },
+    phone: {
+        type: String, 
+        default: ""
+    },
+    zip: {
+        type: String, 
+        default: ""
+    }
+});
+
+// static method to login user
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+}
+
+module.exports = mongoose.model('User', userSchema);
---
+++ routes/userRoute.js
@@ -0,0 +1,183 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const { requireAuth } = require('../middleware/authMiddleware');
+
+const bcrypt = require('bcryptjs');
+const {registerValidation, loginValidation, deleteValidation} = require('../validation')
+
+//user profile page
+router.get('/', requireAuth, (req, res) =>  {
+    const token = req.cookies.jwt;
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    var userId = decoded.id;
+    console.log(userId);
+    // Fetch the user by id
+    User.findOne({ _id: userId }).then((user) => {
+        if(user) {
+            res.render('result', { user });
+        }
+    });
+});
+
+//creating a token used for the user in the current session
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.TOKEN_SECRET, {expiresIn: maxAge});
+}
+
+//register a user
+router.post('/register', async (req, res) => {
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Checking if the user is already in the db.
+    const usernameExists = await User.findOne({email: req.body.username});
+    if(usernameExists) return res.status(400).send('username already exists');
+
+    //Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+
+    //Create new user.
+    const user = new User({
+        username: req.body.username,
+        name: req.body.name,
+        type: req.body.type,
+        bio: req.body.bio,
+        email: req.body.email,
+        password: hashedPassword,
+        phone: req.body.phone,
+        zip: req.body.zip
+    });
+
+    try{
+        const savedUser = await user.save();
+        const token = createToken(savedUser._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.redirect('/');
+        //res.send({user: user._id});
+    }catch(err){
+        res.status(400).send(err);
+    }
+
+});
+
+//Delete
+router.post('/delete', requireAuth, async (req,res) => {
+    const {error} = deleteValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Checking if email exists
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Email is not found.');
+
+    //Check if password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Invalid password');
+
+    //Deleting user
+    User.findOneAndRemove({email: req.body.email}, function(err) {
+        if(!err) {
+            res.redirect('/logout');
+        }
+        else {
+            res.status(400).send('Error');
+        }
+    });
+});
+
+//Edit profile
+router.post('/edit_profile', requireAuth, async (req, res) => { 
+    const token = req.cookies.jwt;
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    var userId = decoded.id;
+    if(req.body.password){
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        User.findOneAndUpdate({_id: userId}, {$set:{password: hashedPassword}}, {new: true}, (err, doc) => {
+            if(err){
+                console.log("Something went wrong");
+            }
+            console.log("Pass updated");
+        });
+    }
+    if(req.body.name){
+        User.findOneAndUpdate({_id: userId}, {$set:{name: req.body.name}}, {new: true}, (err, doc) => {
+            if(err){
+                console.log("Something went wrong");
+            }
+            console.log("Name updated");
+        });
+    }
+    if(req.body.type){
+        User.findOneAndUpdate({_id: userId}, {$set:{type: req.body.type}}, {new: true}, (err, doc) => {
+            if(err){
+                console.log("Something went wrong");
+            }
+            console.log("Type updated");
+        });
+    }
+    if(req.body.bio){
+        User.findOneAndUpdate({_id: userId}, {$set:{bio: req.body.bio}}, {new: true}, (err, doc) => {
+            if(err){
+                console.log("Something went wrong");
+            }
+            console.log("Bio updated");
+        });
+    }
+    if(req.body.email){
+        User.findOneAndUpdate({_id: userId}, {$set: {email: req.body.email}}, {new: true}, (err, doc) => {
+            if(err){
+                console.log("Something went wrong");
+            }
+            console.log("Email updated.");
+        });
+    }
+    if(req.body.phone){
+        User.findOneAndUpdate({_id: userId}, {$set:{phone: req.body.phone}}, {new: true}, (err, doc) => {
+            if(err){
+                console.log("Something went wrong");
+            }
+            console.log("Phone updated");
+        });
+
+    }
+    if(req.body.zip){
+        User.findOneAndUpdate({_id: userId}, {$set:{zip: req.body.zip}}, {new: true}, (err, doc) => {
+            if(err){
+                console.log("Something went wrong");
+            }
+            console.log("ZIP updated");
+        });
+    }
+    res.redirect('/user');
+
+
+});
+
+//Login
+router.post('/login', async (req,res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.login(username, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        console.log(user._id);
+        res.redirect('/');
+    }
+    catch (err) {
+        res.status(400).json({});
+    }
+});
+
+
+//Log out
+router.get('/logout', async (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
+});
+
+
+module.exports = router;
